171514d5908f76d1548a407d569fd06cafab5bdd
fome_team <teamfomee@gmail.com>
2019-03-29 12:32:03 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+function scrollFooter(scrollY, heightFooter) {
+    console.log(scrollY);
+    console.log(heightFooter);
+
+    if (scrollY >= heightFooter) {
+        $("footer").css({
+            bottom: "0px"
+        });
+    } else {
+        $("footer").css({
+            bottom: "-" + heightFooter + "px"
+        });
+    }
+}
+
+$(window).load(function() {
+    var windowHeight = $(window).height(),
+        footerHeight = $("footer").height(),
+        heightDocument =
+            windowHeight + $(".content").height() + $("footer").height() - 20;
+
+    // Setting the size of the element to animate
+    $("#scroll-animate, #scroll-animate-main").css({
+        height: heightDocument + "px"
+    });
+
+    // Defining the size of header and content elements
+    // $('header').css({
+    //     'height' : windowHeight + 'px',
+    //     'line-height' : windowHeight + 'px'
+    // });
+
+    $(".wrapper-parallax").css({
+        "margin-top": windowHeight + "px"
+    });
+
+    scrollFooter(window.scrollY, footerHeight);
+
+    // ao dar rolagem
+    window.onscroll = function() {
+        var scroll = window.scrollY;
+
+        $("#scroll-animate-main").css({
+            top: "-" + scroll + "px"
+        });
+
+        // $('header').css({
+        //     'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
+        // });
+
+        scrollFooter(scroll, footerHeight);
+    };
+});
+
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
+
+/* Demo purposes only */
+$(".hover").mouseleave(function() {
+    $(this).removeClass("hover");
+});
